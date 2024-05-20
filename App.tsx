20d1c81a5ca7f77fdcd815a3e58bf060454125e4
MadhuVarshaP <madhuvarsha0608@gmail.com>
2024-05-20 21:59:16 +0530

    header created

--- App.tsx
+++ App.tsx
@@ -1,118 +1,204 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
+  FlatList,
+  Image,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TouchableOpacity,
   View,
 } from 'react-native';
+import React, {useState} from 'react';
+// import LinearGradient from 'react-native-linear-gradient';
+import {wp, hp} from './utils/ScreenDimensions';
+import dropdown from './src/assets/images/dropdown.png';
+import bell from './src/assets/images/bell.png';
+import search from './src/assets/images/search.png';
+import settings from './src/assets/images/settings.png';
+import LinearGradient from 'react-native-linear-gradient';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const Building = [
+    {
+      name: 'House',
+    },
+    {
+      name: 'Apartment',
+    },
+    {
+      name: 'Hotel',
+    },
+    {
+      name: 'Villa',
+    },
+    {
+      name: 'Residency',
+    },
+    {
+      name: 'Hostel',
+    },
+  ];
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const [active, setActive] = useState('House');
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.location}>Location</Text>
+      <View style={styles.header}>
+        <View style={styles.chooselocation}>
+          <Text style={styles.place}>Bangalore</Text>
+          <Image source={dropdown} style={styles.dropdown} />
         </View>
-      </ScrollView>
+        <Image source={bell} style={styles.bell} />
+      </View>
+      <View style={styles.headertitle}>
+        <View style={styles.inputcontainer}>
+          <Image source={search} style={styles.search} />
+          <Text style={styles.searchinput}>Search address, or near you</Text>
+        </View>
+        <LinearGradient
+          colors={['#A0DAFB', '#65BCEE']}
+          start={{x: 0, y: 0}}
+          end={{x: 0.5, y: 0.5}}
+          style={styles.settingsbox}>
+          <Image source={settings} style={styles.settings} />
+        </LinearGradient>
+        {/* <View style={styles.settingsbox}>
+          <Image source={settings} style={styles.settings} />
+        </View> */}
+      </View>
+      <FlatList
+        data={Building}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            style={
+              active === item.name ? styles.buildlistActive : styles.buildlist
+            }
+            onPress={() => setActive(item.name)}>
+            <Text
+              style={
+                active === item.name ? {color: 'white'} : {color: '#71797E'}
+              }>
+              {item.name}
+            </Text>
+          </TouchableOpacity>
+        )}
+      />
+      <View style={styles.headingBox}>
+        <Text style={styles.heading1}>Near from you</Text>
+        <Text style={styles.subheading1}>See more</Text>
+      </View>
     </SafeAreaView>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    width: wp(100),
+    height: hp(100),
+    color: 'black',
+    fontFamily: 'JosefinSans-Medium',
+  },
+  location: {
+    fontFamily: 'JosefinSans-Medium',
+    margin: 15,
+    fontSize: 14,
+    color: 'gray',
+  },
+  chooselocation: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  place: {
+    color: 'black',
+    fontFamily: 'JosefinSans-Medium',
+    fontSize: 18,
+    marginHorizontal: 20,
+  },
+  dropdown: {
+    height: 18,
+    width: 18,
+  },
+  bell: {
+    width: 25,
+    height: 25,
+    marginHorizontal: 20,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  search: {
+    height: 20,
+    width: 20,
+    paddingRight: 20,
+  },
+  inputcontainer: {
+    backgroundColor: '#E6E7E7',
+    width: 260,
+    margin: 20,
+    padding: 10,
+    flexDirection: 'row',
+    borderRadius: 10,
+  },
+  searchinput: {
+    color: '#4B4B4B',
+    paddingLeft: 10,
+  },
+  settings: {
+    width: 20,
+    height: 20,
+  },
+  settingsbox: {
+    backgroundColor: '#BFEFFF',
+    padding: 10,
+    width: 40,
+    height: 40,
+    borderRadius: 10,
+  },
+  headertitle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginRight: 20,
+  },
+  buildlist: {
+    // width: 80,
+    height: 45,
+    marginVertical: 10,
+    marginHorizontal: 15,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    backgroundColor: '#E5E4E2',
+  },
+  buildlistActive: {
+    // width: 80,
+    height: 45,
+    marginVertical: 10,
+    marginHorizontal: 15,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    backgroundColor: '#65BCEE',
+  },
+  headingBox: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+  },
+  heading1: {
+    color: 'black',
+    fontSize: 18,
+  },
+  subheading1: {
+    color: '#71797E',
+    fontSize: 14,
+  },
+});
